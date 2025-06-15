41445b430a4fb0411fac5b2838242bc6adb3822c
zacharywong34174 <zacharywong34174@gmail.com>
2025-06-15 00:32:15 -0400

    Moving stickmen

--- my-project/first.py
+++ my-project/first.py
@@ -16,6 +16,31 @@ Draw border then fill
 
 .arrange()
 .arrange_in_grid()
+
+rate_func
+AnimationGroup and lag_ratio
+
+move_to_target!!!
+    mob.generate_target()
+    modify mob.target()
+    animate with MoveToTarget(mob)
+restore!!!
+    mob.save_state()
+    modify mob
+    animate with Restore(mob)
+
+UpdateFromAlphaFunc(mobject, function, run_time)
+Where function takes mobject as first arg, alpha completion ratio second arg
+
+
+Updater functions, inline and with dt 
+
+Mobject updaters 
+Scene updaters
+
+Suspend updating, resume updating
+
+ValueTracker
 '''
 
 
@@ -111,7 +136,7 @@ class TestScene(Scene):
 
         self.wait(1)
 
-        text1 = Tex(r"Let's put \emph{balls} in the middle of each of these lines.", substrings_to_isolate=["balls"]).to_edge(UP).shift(DOWN)
+        text1 = Tex(r"Let's put \textit{balls} in the middle of each of these lines.", substrings_to_isolate=["balls"]).to_edge(UP).shift(DOWN)
         text1.set_color_by_tex("balls", YELLOW)
         gigachad = ImageMobject("media\\images\\first\\gigachad.jpg").scale(0.4)
         self.play(FadeTransform(text, text1, run_time=2), transformation_group_2.animate.arrange(RIGHT, buff=1.2))
@@ -138,6 +163,7 @@ class TestScene(Scene):
 
         '''
         Question should be: What happens to the height of the balls as they go to the right? (word in small pieces, though)
+        Which would be the easiest to walk across??
         '''
 
         text3b = Tex(r"What do you notice about the lines?").to_edge(UP).shift(DOWN)
@@ -147,10 +173,61 @@ class TestScene(Scene):
         self.play(FadeTransform(text2, text3a, run_time=2), Uncreate(VGroup(*list_dots), run_time=1))
         self.play(FadeIn(text3b, run_time=2))
 
-        self.wait(4)
+        self.wait(3)
 
-        '''
-        Now let's temporarily take away the coordinate plane.
-        '''
+        text4a = Tex(r"Fine, let's ask a different question.").scale(0.8).to_edge(UP)
+        self.play(FadeTransform(VGroup(text3a, text3b), text4a, run_time=2), Uncreate(VGroup(*list_dots)))
 
+        # Define the head as a circle
+        head = Circle(radius=0.2, color=WHITE)
+
+        # Define the body as a line
+        body = Line(ORIGIN, DOWN, color=WHITE, stroke_width=3)
+        body.next_to(head, DOWN, buff=0)
+
+        # Define the arms as lines
+        arm1 = Line(UP*0.5, LEFT * 0.75 * 0.5, color=WHITE, stroke_width=3)
+        arm1.next_to(body.get_center(), LEFT, buff=0)
+        arm2 = Line(UP*0.5, RIGHT * 0.75 * 0.5, color=WHITE, stroke_width=3)
+        arm2.next_to(body.get_center(), RIGHT, buff=0)
+
+        # # Define the legs as lines
+        leg1 = Line(DOWN * 0.5, LEFT * 0.75 * 0.5, color=WHITE, stroke_width=3)
+        leg2 = Line(DOWN * 0.5, RIGHT * 0.75 * 0.5, color=WHITE, stroke_width=3)
+        leg2.next_to(body.get_center()+body.get_length()/2*DOWN*1.41, LEFT, buff=0)
+        leg1.next_to(body.get_center()+body.get_length()/2*DOWN*1.41, RIGHT, buff=0)
+
+        # # Group the parts of the figure
+        # stick_figure = VGroup(head, body, arm1, arm2, leg1, leg2)
+        # stick_figure.arrange_in_grid(row_spacing=0.1, col_spacing=0.1) # Arrange the parts
+        # stick_figure.scale(1.5) # scale up the figure
+
+        stick_figure = VGroup(head, body, arm1, arm2, leg1, leg2).scale(0.7)
+        stick_figure_copy1 = stick_figure.copy()
+        stick_figure_copy2 = stick_figure.copy()
+
+        #can probably use an ANIMATE GROUP for this
+        self.play(stick_figure.animate.move_to(list_of_lines[0].get_start()).rotate(-PI/4).shift(UP*0.3+RIGHT*0.8), 
+            stick_figure_copy1.animate.move_to(list_of_lines[2].get_start()).rotate(PI/4).shift(UP*0.8+LEFT*0.3), 
+            stick_figure_copy2.animate.move_to(line_centers[1]).shift(UP*body.get_length()+LEFT*list_of_lines[1].get_length()/2+RIGHT*0.2)
+        )
+        self.wait(3)
+        text4b = Tex(r"How do these \textit{alpha males} \textbf{move} across these lines?").next_to(text4a, DOWN)
+        self.play(FadeIn(text4b))
+        self.wait(5)
+
+        # def stick_updater(mobject, dt):
+        #     bottom = mobject.get_bottom()
+        
+        # stick_figure.add_updater(
+        #     lambda mob: mob.move_to(list_of_lines[0].get_start()).shift(list_of_lines[0].get_start()-mob.get_bottom())
+        # )
+
+        self.play(
+            stick_figure.animate.move_to(list_of_lines[0].get_end()).shift(UP*0.63+RIGHT*0.25),
+            stick_figure_copy1.animate.move_to(list_of_lines[2].point_from_proportion(1)).shift(UP*0.27+LEFT*0.65),
+            stick_figure_copy2.animate.move_to(list_of_lines[1].point_from_proportion(1)).shift(UP*0.7+LEFT*0.28)
+        )
+
+        self.wait(5)
     
